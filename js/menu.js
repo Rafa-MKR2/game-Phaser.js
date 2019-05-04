6743c76b2029754d38c4085caef0018e75f9c6e5
Rafa-MKR2 <rafaeldocarmo2010@gmail.com>
2019-05-04 13:35:03 -0300

    justando controles

--- js/menu.js
+++ js/menu.js
@@ -32,10 +32,10 @@ console.log(porcentagem(10,window.innerWidth))
 
     // Controles Up / Down
     this.up = game.input.keyboard.addKey(Phaser.Keyboard.UP);
-    this.up.onDown.add(up, this);
+    this.up.onDown.add(this.upSeleciona, this);
 
     this.down = game.input.keyboard.addKey(Phaser.Keyboard.DOWN);
-    this.down.onDown.add(down, this);
+    this.down.onDown.add(this.downSeleciona, this);
 
     this.enter = game.input.keyboard.addKey(Phaser.Keyboard.ENTER);
     this.enter.onDown.add(this.confirmar, this);
@@ -48,55 +48,34 @@ console.log(porcentagem(10,window.innerWidth))
     
       game.add.button(
         porcentagem(85,window.innerWidth),
-        porcentagem(70,window.innerHeight), 'buttonB', actionOnClick, this, 4, 2, 0);
+        porcentagem(70,window.innerHeight), 'buttonB', actionOnClick, this, 1, 1, 0);
+
+
+
+
+     game.add.button(
+        porcentagem(10,window.innerWidth),
+        porcentagem(50,window.innerHeight), 'buttonUp', this.upSeleciona,this);
 
       game.add.button(
-        porcentagem(10,window.innerWidth),
-        porcentagem(50,window.innerHeight), 'buttonUp', up, this, 4, 2, 0);
-      
-       game.add.button(
           porcentagem(15,window.innerWidth),
-          porcentagem(65,window.innerHeight), 'buttonRight', actionOnClick, this, 4, 2, 0);
+          porcentagem(65,window.innerHeight), 'buttonRight', actionOnClick,this);
        
-        game.add.button(
+      game.add.button(
             porcentagem(3,window.innerWidth),
-            porcentagem(65,window.innerHeight), 'buttonLeft', actionOnClick, this, 4, 2, 0);
+            porcentagem(65,window.innerHeight), 'buttonLeft', actionOnClick, this);
        
-        game.add.button(
+      game.add.button(
               porcentagem(10,window.innerWidth),
-              porcentagem(75,window.innerHeight), 'buttonDown', down, this, 4, 2, 0);
-        
+              porcentagem(75,window.innerHeight), 'buttonDown',this.downSeleciona, this)
+             
 
     function actionOnClick () {
 
     console.log('teste')
   
   }
-    function up(){
-      alert('cima')
-
-        this.select = this.select-1;
-
-        if(this.select<0){
-            this.select=2;
-            return;
-         } 
-      
-    }
-
-    function down(){
-      alert('baixo')
-        this.select=  this.select+1;
-
-        if(this.select>2) {
-            this.select=0;
-
-            return;
-        } 
-
-
-    }
-
+   
 
 
 
@@ -110,35 +89,42 @@ console.log(porcentagem(10,window.innerWidth))
 },
 
 
-checked : function(){
-    this.menu.forEach(item => {
-        if(item==this.menu[this.select]){
+    checked : function(){
+        this.menu.forEach(item => {
+            if(item==this.menu[this.select]){
 
-            this.menu[this.select].fill = 'red';
-        }else{
-        item.fill = '#fff'
+                this.menu[this.select].fill = 'red';
+            }else{
+            item.fill = '#fff'
 
-        }
-    })
-},
+            }
+        })
+    },
 
+    upSeleciona: function(){
+      return this.select=this.select-1 <0? this.select=2 : this.select-1;
+    },
 
-confirmar : function(){
+    downSeleciona: function(){
+     return this.select= this.select+1 >2?  this.select=0: this.select+1;
+    },
 
-  var selecione = this.menu[this.select].text;
-  if(selecione=='Novo Game'){
-   
-    music.pause();
-    return game.state.start('stage1');
+    confirmar : function(){
+    console.log('teste confirme')
+      var selecione = this.menu[this.select].text;
+      if(selecione=='Novo Game'){
+      
+        music.pause();
+        return game.state.start('stage1');
 
-  }else if(selecione=='Recordes'){
-   
-    return game.state.start('recordes')
+      }else if(selecione=='Recordes'){
+      
+        return game.state.start('recordes')
 
-  }else if(selecione=='Options'){
-    return game.state.start('options');
+      }else if(selecione=='Options'){
+        return game.state.start('options');
 
-  }
-}
+      }
+    }
 
 }
